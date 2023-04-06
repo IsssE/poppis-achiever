b9f3f705ad08a4c7e7b5bda62c2d02be98c74ab7
IsssE <tommi.linnamaa@gmail.com>
2023-04-06 23:36:53 +0300

    env stuff

--- client/pulu/src/lib/api.ts
+++ client/pulu/src/lib/api.ts
@@ -1,11 +1,13 @@
 // Feel like there should be a better way to do this
 
 import { GraphQLClient } from 'graphql-request';
+import { PUBLIC_API_URL } from '$env/static/public';
+
 import Cookies from 'js-cookie';
 const jwt = Cookies.get('jwt');
 // https://localhost:7029/ when local
-// https://localhost:7000/ when container
-const clientGQL = new GraphQLClient('https://localhost:7029/graphql', {
+// http://localhost:7000/ when container
+const clientGQL = new GraphQLClient(`${PUBLIC_API_URL}/graphql`, {
 	credentials: 'include',
 	mode: 'cors'
 }).setHeader('Authorization', jwt ? jwt : '');
--- client/pulu/src/lib/components/header.svelte
+++ client/pulu/src/lib/components/header.svelte
@@ -4,9 +4,9 @@
 	let showModal = false;
 	let userName: string | undefined = undefined;
 
-	userStore.subscribe(user => {
-		userName = user.currentUser.displayName
-	})
+	userStore.subscribe((user) => {
+		userName = user.currentUser.displayName;
+	});
 
 	const closeLogin = () => {
 		showModal = false;
@@ -16,13 +16,12 @@
 <div class="flex bg-gray-400 h-10">
 	<h1 class="flex-auto text-3xl font-bold text-center ml-20">Pulu</h1>
 	{#if userName}
-	<div class= "flex-none py-2 px-5 w-15 ">{userName}</div>
+		<div class="flex-none py-2 px-5 w-15 ">{userName}</div>
 	{:else}
-	<button on:click={() => (showModal = true)} type="button" class="flex-none px-5 w-15 ">
-		Login</button
+		<button on:click={() => (showModal = true)} type="button" class="flex-none px-5 w-15 ">
+			Login</button
 		>
 	{/if}
-
 </div>
 
 {#if showModal}
